41196d933585a2e9a5e4146b8a4b1f52273c6491
nazimboudeffa <boudeffa@gmail.com>
2023-05-02 23:41:55 +0200

    Added Products and Card

---
+++ components/Products.tsx
@@ -0,0 +1,18 @@
+import Card from './Card'
+
+function Products () {
+    return (
+        <div className="mx-auto py-16 px-4 sm:py-24 sm:px-6">    
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-x-6 gap-y-10">
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
+        </div>
+    )
+}
+
+export default Products
---
+++ pages/products.tsx
@@ -0,0 +1,13 @@
+import Navbar from '../components/Navbar'
+import Products from '../components/Products'
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Products />
+      <Footer />
+    </>
+  )
+}
