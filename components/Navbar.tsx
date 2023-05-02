46579fb96bceb612232764db8a7399616e3e2a00
nazimboudeffa <boudeffa@gmail.com>
2023-05-02 23:56:43 +0200

    Updated the Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,17 +10,7 @@ function Navbar () {
                 </label>
                 <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                     <li><Link href="/products">Products</Link></li>
-                    <li tabIndex={0}>
-                    <a className="justify-between">
-                        Parent
-                        <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
-                    </a>
-                    <ul className="p-2">
-                        <li><a>Submenu 1</a></li>
-                        <li><a>Submenu 2</a></li>
-                    </ul>
-                    </li>
-                    <li><a>Item 3</a></li>
+                    <li><a>Contact</a></li>
                 </ul>
                 </div>
                 <Link href="/" className="btn btn-ghost normal-case text-xl">daisyUI</Link>
@@ -28,17 +18,7 @@ function Navbar () {
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
                 <li><Link href="/products">Products</Link></li>
-                <li tabIndex={0}>
-                    <a>
-                    Parent
-                    <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-                    </a>
-                    <ul className="p-2">
-                    <li><a>Submenu 1</a></li>
-                    <li><a>Submenu 2</a></li>
-                    </ul>
-                </li>
-                <li><a>Item 3</a></li>
+                <li><a>Contact</a></li>
                 </ul>
             </div>
             <div className="navbar-end flex-none">
