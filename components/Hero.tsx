38edd1c7b430137c11b0bf02c208cfd55129be8a
nazimboudeffa <boudeffa@gmail.com>
2023-05-04 01:50:45 +0200

    Fixing the sign in

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ function Hero () {
     const signIn = async () => {
         if (!email) alert('Please enter a valid email')
         try {
-            let { data, error } = await supabase.auth.signIn({
+            let { data, error } = await supabase.auth.signInWithOtp({
                 email: email,
                 options: {
                   emailRedirectTo: process.env.NEXT_PUBLIC_SITE_URL,
