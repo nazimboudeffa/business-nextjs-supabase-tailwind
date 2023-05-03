1f7d26dd2b60da0a4122715874c113c9da21cd4e
nazimboudeffa <boudeffa@gmail.com>
2023-05-04 01:10:31 +0200

    Fixing the sign in

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -38,20 +38,21 @@ function Hero () {
             </div>
             </>
         )
-    }
-    return (
-        <div className="hero min-h-screen" style={{ backgroundImage: `url("/images/stock/photo-1507358522600-9f71e620c44e.jpg")` }}>
-            <div className="hero-overlay bg-opacity-60"></div>
-            <div className="hero-content text-center text-neutral-content">
-                <div className="max-w-md">
-                <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                <input type="text" placeholder="Type your Email" className="input input-bordered input-primary w-full max-w-xs" onChange={e => setEmail(e.target.value)} />
-                <button className="btn btn-primary" onClick={signIn}>Get Started</button>
+    } else {
+        return (
+            <div className="hero min-h-screen" style={{ backgroundImage: `url("/images/stock/photo-1507358522600-9f71e620c44e.jpg")` }}>
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md">
+                    <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                    <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <input type="text" placeholder="Type your Email" className="input input-bordered input-primary w-full max-w-xs" onChange={e => setEmail(e.target.value)} />
+                    <button className="btn btn-primary" onClick={signIn}>Get Started</button>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Hero
