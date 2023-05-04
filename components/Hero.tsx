da19b99061039ed78367a0fc59d6e7e4098770a9
nazimboudeffa <boudeffa@gmail.com>
2023-05-04 02:29:42 +0200

    Fixing the sign in

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,10 +5,7 @@ function Hero () {
     const [email, setEmail] = useState('')
     const [submitted, setSubmitted] = useState(false)
     const getURL = () => {
-        let url =
-          process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-          process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-          'http://localhost:3000/';
+        let url = process.env.NEXT_PUBLIC_SITE_URL
         // Make sure to include `https://` when not localhost.
         url = url.includes('http') ? url : `https://${url}`;
         // Make sure to including trailing `/`.
