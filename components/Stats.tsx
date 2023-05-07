86dfab44d1cac841391a609c2f714f8f086b3a0b
nazimboudeffa <boudeffa@gmail.com>
2023-05-08 00:33:29 +0200

    Added active users stats

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -1,16 +1,17 @@
+import { useState } from "react"
+
 function Stats () {
+    const [activeUsers, setActiveUsers] = useState(101)
     return (
         <section className="bg-white">
         <div className="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 md:py-16 lg:px-8">
             <div className="mx-auto max-w-3xl text-center">
             <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl">
-                Trusted by eCommerce Businesses
+                Trusted by {activeUsers} active users
             </h2>
 
             <p className="mt-4 text-gray-500 sm:text-xl">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione dolores
-                laborum labore provident impedit esse recusandae facere libero harum
-                sequi.
+                On the general dashboard you will find everything to manage your business
             </p>
             </div>
 
