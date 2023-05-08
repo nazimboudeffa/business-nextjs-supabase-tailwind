a732e6d0353c93eb9dde7ecaf2cbcbbe91f6ea60
nazimboudeffa <boudeffa@gmail.com>
2023-05-08 05:20:41 +0200

    Little fix for the Nevbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,10 +8,13 @@ function Navbar () {
                 <label tabIndex={0} className="btn btn-ghost lg:hidden">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                 </label>
-                <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                    <li><Link href="/services">Services</Link></li>
-                    <li><a>Contact</a></li>
-                </ul>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><Link href="/dashboard">Home</Link></li>
+                        <li><a>About</a></li>
+                        <li><Link href="/services">Services</Link></li>
+                        <li><a>Pricing</a></li>
+                        <li><a>Contact</a></li>
+                    </ul>
                 </div>
                 <Link href="/" className="btn btn-ghost normal-case text-xl">Allbizzz</Link>
             </div>
