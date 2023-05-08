7c45719e6f8d76a75bc0b35cafd845bbd8f3cc07
nazimboudeffa <boudeffa@gmail.com>
2023-05-08 02:11:15 +0200

    Removed account setting from the Sidebar

--- components/Sidenav.tsx
+++ components/Sidenav.tsx
@@ -166,22 +166,12 @@ function Sidenav () {
                 className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
               >
                 <div className="flex items-center gap-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5 opacity-75"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-width="2"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-                    />
+                  
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z" />
                   </svg>
-      
-                  <span className="text-sm font-medium"> Account </span>
+        
+                  <span className="text-sm font-medium"> Options </span>
                 </div>
       
                 <span className="shrink-0 transition duration-300 group-open:-rotate-180">
@@ -245,29 +235,6 @@ function Sidenav () {
                   <span className="text-sm font-medium"> Security </span>
                 </a>
       
-                <form action="/logout">
-                  <button
-                    type="submit"
-                    className="flex w-full items-center gap-2 rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-5 w-5 opacity-75"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      stroke-width="2"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-                      />
-                    </svg>
-      
-                    <span className="text-sm font-medium"> Logout </span>
-                  </button>
-                </form>
               </nav>
             </details>
           </nav>
@@ -276,7 +243,7 @@ function Sidenav () {
         <div className="sticky inset-x-0 bottom-0 border-t border-gray-100">
           <a href="#" className="flex items-center gap-2 bg-white p-4 hover:bg-gray-50">
             <div>
-              <p className="text-xs">
+              <p className="text-sm">
                 <strong className="block font-medium">Need help ?</strong>
               </p>
             </div>
