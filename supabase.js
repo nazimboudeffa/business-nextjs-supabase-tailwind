864a178e1e1206fb5a96d24327af17ef41f264b0
nazimboudeffa <boudeffa@gmail.com>
2023-05-09 01:01:26 +0200

    Fixing some errors

--- supabase.js
+++ supabase.js
@@ -1,5 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
+console.log(process.env.NEXT_PUBLIC_SUPABASE_URL)
+console.log(process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+
 const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
 
 export { supabase }
