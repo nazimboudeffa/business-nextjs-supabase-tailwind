24312a0040fb99f5e8214d450f91cfc553393c07
nazimboudeffa <boudeffa@gmail.com>
2023-05-08 06:22:56 +0200

    Little typo

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -44,7 +44,7 @@ export default function Dashboard () {
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                 </label>
                 <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                    <li><Link href="/service">Service</Link></li>
+                    <li><Link href="/services">Services</Link></li>
                     <li><a>Contact</a></li>
                 </ul>
                 </div>
