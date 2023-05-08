48722339e0d6fe4f205ea74d970b4cbe3ccb5a34
nazimboudeffa <boudeffa@gmail.com>
2023-05-08 23:51:38 +0200

    Fixing some errors

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { supabase } from '../supabase'
+import { supabase } from '@/supabase'
 import { useRouter } from 'next/router'
 
 function Login () {
