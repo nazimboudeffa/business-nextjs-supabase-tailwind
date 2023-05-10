fe94885a3a77549dc43e05d70e652b39ec8c0eae
nazimboudeffa <boudeffa@gmail.com>
2023-05-10 20:55:50 +0200

    Fixing types

--- components/NavbarSession.tsx
+++ components/NavbarSession.tsx
@@ -1,8 +1,9 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useSupabaseClient} from '@supabase/auth-helpers-react'
+import { Session } from '@supabase/supabase-js'
 
-function NavbarSession ({ session } : { session : null }) {
+function NavbarSession ({ session } : { session : Session }) {
     const supabase = useSupabaseClient()
     const router = useRouter();
 
