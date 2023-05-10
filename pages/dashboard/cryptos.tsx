1e90f08c2e0b96d3adeb6ff60b0fd9e237bbfcf8
nazimboudeffa <boudeffa@gmail.com>
2023-05-10 20:38:01 +0200

    Removed ApexChart charts components

--- pages/dashboard/cryptos.tsx
+++ pages/dashboard/cryptos.tsx
@@ -1,14 +1,9 @@
 //import Donut from '../../components/Donut'
-import ChartBar from '@/components/ChartBar'
-import ChartDonut from '@/components/ChartDonut'
-import Navbar from '@/components/Navbar'
-
 function Cryptos() {
 
   return (
     <>
-    <ChartBar />
-    <ChartDonut />
+
     </>
   )
 }
